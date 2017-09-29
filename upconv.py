d8276fe3a034a2c5010d1d4b36cc67e20677033f
Saumitra Mishra <Saumitra@Saumitras-Air.home>
2017-09-29 01:01:00 +0100

    removed print statements

--- upconv.py
+++ upconv.py
@@ -21,17 +21,17 @@ def architecture_upconv(input_var, input_shape):
                   nonlinearity=lasagne.nonlinearities.leaky_rectify,
                   W=lasagne.init.Orthogonal())
     net['uc1'] = TransposedConv2DLayer(net['rs1'], num_filters= 16, **kwargs)
-    print(net['uc1'].output_shape)    
+    #print(net['uc1'].output_shape)    
     net['uc2'] = TransposedConv2DLayer(net['uc1'], num_filters= 8, **kwargs)
-    print(net['uc2'].output_shape)    
+    #print(net['uc2'].output_shape)    
     net['uc3'] = TransposedConv2DLayer(net['uc2'], num_filters= 4, **kwargs)
     net['uc4'] = TransposedConv2DLayer(net['uc3'], num_filters= 2, **kwargs)
     net['uc5'] = TransposedConv2DLayer(net['uc4'], num_filters= 1, **kwargs)
 
     # slicing the output to 115 x 80 size
-    print(net['uc5'].output_shape)    
+    #print(net['uc5'].output_shape)    
     net['s1'] = lasagne.layers.SliceLayer(net['uc5'], slice(0, 115), axis=-2)
     net['out'] = lasagne.layers.SliceLayer(net['s1'], slice(0, 80), axis=-1)
-    print(net['out'] .output_shape)
+    #print(net['out'] .output_shape)
     
     return net['out']
