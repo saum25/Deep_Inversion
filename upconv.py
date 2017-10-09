59b4a6569802de8dd2c419a54afc1eace87ad173
Saumitra Mishra <Saumitra@Saumitras-MacBook-Air.local>
2017-10-09 17:30:46 +0100

    experimenting different architectures for fc8 inversion

--- upconv.py
+++ upconv.py
@@ -12,8 +12,9 @@ def architecture_upconv_fc8(input_var, input_shape):
     net = {}
     
     net['data'] = InputLayer(input_shape, input_var)
-    #net['fc1'] = batch_norm(DenseLayer(net['data'], num_units=64, W=lasagne.init.HeNormal(), nonlinearity=lasagne.nonlinearities.elu))
-    net['fc2'] = batch_norm(DenseLayer(net['data'], num_units=256, W=lasagne.init.HeNormal(), nonlinearity=lasagne.nonlinearities.elu))
+    net['fc1'] = batch_norm(DenseLayer(net['data'], num_units=64, W=lasagne.init.HeNormal(), nonlinearity=lasagne.nonlinearities.elu))
+    print(net['fc1'].output_shape)
+    net['fc2'] = batch_norm(DenseLayer(net['fc1'], num_units=256, W=lasagne.init.HeNormal(), nonlinearity=lasagne.nonlinearities.elu))
     print(net['fc2'].output_shape)
     net['rs1'] = ReshapeLayer(net['fc2'], (32, 16, 4, 4)) # assuming that the shape is batch x depth x row x columns
     kwargs = dict(nonlinearity=lasagne.nonlinearities.elu,
