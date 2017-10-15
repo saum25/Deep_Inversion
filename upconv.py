d9fb05ec142f8b8f6fa77f4716f49a1c32bdf656
Saumitra Mishra <Saumitra@Saumitras-MacBook-Air.local>
2017-10-15 11:46:57 +0100

    experimenting with architectures for c5_mp6 - a7

--- upconv.py
+++ upconv.py
@@ -119,21 +119,21 @@ def architecture_upconv_c5_mp6(input_var, input_shape):
     print(net['c2'].output_shape)
     net['c3'] = batch_norm(Conv2DLayer(net['c2'], num_filters= 64, filter_size= 3, stride = 1, pad=1, **kwargs))
     print(net['c3'].output_shape)
-    net['c4'] = batch_norm(Conv2DLayer(net['c3'], num_filters= 64, filter_size= 3, stride = 1, pad=1, **kwargs))
-    print(net['c4'].output_shape)
+    '''net['c4'] = batch_norm(Conv2DLayer(net['c3'], num_filters= 64, filter_size= 3, stride = 1, pad=1, **kwargs))
+    print(net['c4'].output_shape)'''
     
     # Bunch of transposed convolution layers  
-    net['uc1'] = batch_norm(TransposedConv2DLayer(net['c4'], num_filters= 64, filter_size= 4, stride = 2, crop=1, **kwargs))
+    net['uc1'] = batch_norm(TransposedConv2DLayer(net['c3'], num_filters= 32, filter_size= 4, stride = 2, crop=1, **kwargs))
     print(net['uc1'].output_shape)
     '''net['c1'] = batch_norm(Conv2DLayer(net['uc1'], num_filters= 32, filter_size= 3, stride = 1, pad=1, **kwargs))
     print(net['c1'].output_shape)'''
     
-    net['uc2'] = batch_norm(TransposedConv2DLayer(net['uc1'], num_filters= 32, filter_size= 4, stride = 2, crop=1, **kwargs))
+    net['uc2'] = batch_norm(TransposedConv2DLayer(net['uc1'], num_filters= 16, filter_size= 4, stride = 2, crop=1, **kwargs))
     print(net['uc2'].output_shape)
     '''net['c2'] = batch_norm(Conv2DLayer(net['uc2'], num_filters= 16, filter_size= 3, stride = 1, pad=1, **kwargs))
     print(net['c2'].output_shape)''' 
     
-    net['uc3'] = batch_norm(TransposedConv2DLayer(net['uc2'], num_filters= 16, filter_size= 4, stride = 2, crop=1, **kwargs))
+    net['uc3'] = batch_norm(TransposedConv2DLayer(net['uc2'], num_filters= 8, filter_size= 4, stride = 2, crop=1, **kwargs))
     print(net['uc3'].output_shape)
     '''net['c3'] = batch_norm(Conv2DLayer(net['uc3'], num_filters= 8, filter_size= 3, stride = 1, pad=1, **kwargs))
     print(net['c3'].output_shape)'''
