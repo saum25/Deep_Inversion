20deaa06fc5fb61d57699c172677dfdfb6d169a3
Saumitra Mishra <Saumitra@Saumitras-MacBook-Air.local>
2017-10-15 16:59:56 +0100

    experimenting with architectures for inverting conv 4 - a1

--- upconv.py
+++ upconv.py
@@ -119,13 +119,13 @@ def architecture_upconv_c5_mp6(input_var, input_shape):
     print(net['c2'].output_shape)
     net['c3'] = batch_norm(Conv2DLayer(net['c2'], num_filters= 64, filter_size= 3, stride = 1, pad=1, **kwargs))
     print(net['c3'].output_shape)
-    net['c4'] = batch_norm(Conv2DLayer(net['c3'], num_filters= 64, filter_size= 3, stride = 1, pad=1, **kwargs))
+    '''net['c4'] = batch_norm(Conv2DLayer(net['c3'], num_filters= 64, filter_size= 3, stride = 1, pad=1, **kwargs))
     print(net['c4'].output_shape)
     net['c5'] = batch_norm(Conv2DLayer(net['c4'], num_filters= 64, filter_size= 3, stride = 1, pad=1, **kwargs))
-    print(net['c5'].output_shape)
+    print(net['c5'].output_shape)'''
     
     # Bunch of transposed convolution layers  
-    net['uc1'] = batch_norm(TransposedConv2DLayer(net['c5'], num_filters= 32, filter_size= 4, stride = 2, crop=1, **kwargs))
+    net['uc1'] = batch_norm(TransposedConv2DLayer(net['c3'], num_filters= 32, filter_size= 4, stride = 2, crop=1, **kwargs))
     print(net['uc1'].output_shape)
     '''net['c1'] = batch_norm(Conv2DLayer(net['uc1'], num_filters= 32, filter_size= 3, stride = 1, pad=1, **kwargs))
     print(net['c1'].output_shape)'''
@@ -164,15 +164,15 @@ def architecture_upconv_conv4(input_var, input_shape):
     print(net['data'].output_shape)
     
     # Bunch of 3 x 3 convolution layers: experimentally we found that, adding 3 conv layers in start than in middle is better: but why?
-    net['c1'] = batch_norm(Conv2DLayer(net['data'], num_filters= 128, filter_size= 3, stride = 1, pad=1, **kwargs))
+    '''net['c1'] = batch_norm(Conv2DLayer(net['data'], num_filters= 128, filter_size= 3, stride = 1, pad=1, **kwargs))
     print(net['c1'].output_shape)   
-    '''net['c2'] = batch_norm(Conv2DLayer(net['c1'], num_filters= 128, filter_size= 3, stride = 1, pad=1, **kwargs))
+    net['c2'] = batch_norm(Conv2DLayer(net['c1'], num_filters= 128, filter_size= 3, stride = 1, pad=1, **kwargs))
     print(net['c2'].output_shape)
     net['c3'] = batch_norm(Conv2DLayer(net['c2'], num_filters= 128, filter_size= 3, stride = 1, pad=1, **kwargs))
     print(net['c3'].output_shape)'''
     
     # Bunch of transposed convolution layers  
-    net['uc1'] = batch_norm(TransposedConv2DLayer(net['c1'], num_filters= 64, filter_size= 4, stride = 2, crop=1, **kwargs))
+    net['uc1'] = batch_norm(TransposedConv2DLayer(net['data'], num_filters= 32, filter_size= 4, stride = 2, crop=1, **kwargs))
     print(net['uc1'].output_shape)
     
     net['uc2'] = batch_norm(TransposedConv2DLayer(net['uc1'], num_filters= 1, filter_size= 4, stride = 2, crop=1, **kwargs))
@@ -183,6 +183,8 @@ def architecture_upconv_conv4(input_var, input_shape):
     print(net['s1'].output_shape)  
     net['out'] = lasagne.layers.SliceLayer(net['s1'], slice(0, 80), axis=-1)
     print(net['out'] .output_shape)
+    
+    print("Number of parameter to be learned: %d" %(lasagne.layers.count_params(net['out'])))
     
     return net['out']
 
