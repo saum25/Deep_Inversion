a48071ea62c44b2d6bf8a5199a7fa5d4e34ff576
Saumitra Mishra <saum.bits@gmail.com>
2017-12-10 00:41:43 +0000

    analysis of mask-based explanations

--- generate.py
+++ generate.py
@@ -224,14 +224,16 @@ def main():
     print('\n===Instance based analysis====\n')
     start_offset = 10
     end_offset = 20 
-    duration = 180  # secs
+    duration =  20  # secs
     mask_threshold = 0.5
     class_threshold = 0.66  # Jan's code
+    error_threshold = 0.5
     file_idx = np.arange(0, len(filelist))    
     hop_size= sample_rate/fps # samples
     dump_path = './audio'   # path to save the reconstructed audio
     pred_before = []    # class predictions before masking
     pred_after = [] # class predictions after masking
+    gen_error = []  # error in generation per instance
     plot_flag = False
     
     for file_instance in file_idx:
@@ -270,7 +272,9 @@ def main():
             pred_before.append(predictions[0][0])
                                
             mel_predictions = np.squeeze(test_fn(scores), axis = 1) # mel_predictions is a 3-d array of shape batch_size x blocklen x n_mels
-            print("NRE in generating the instance: %f" %((util.dist_euclidean(excerpts[excerpt_idx], mel_predictions[0])/N)))
+            error_instance = util.dist_euclidean(excerpts[excerpt_idx], mel_predictions[0])/N
+            gen_error.append(error_instance)
+            print("NRE in generating the instance: %f" %((error_instance)))
                 
             # normalising the inverted mel to create a map, and use the map to cut the section in the input mel
             norm_inv = util.normalise(mel_predictions[0])
@@ -304,16 +308,36 @@ def main():
     # quantify the classification performance after masking
     ground = (np.asarray(pred_before))>class_threshold
     pred = (np.asarray(pred_after))>class_threshold
+    class_change = np.zeros(len(ground))
     count_pass = 0
     count_fail = 0
+    count_cc_lt = 0
+    count_cnc_lt = 0
+    count_cc_gt = 0
+    count_cnc_gt = 0
+    
     for i in range(len(ground)):
         if ground[i]==pred[i]:
             count_pass +=1
+            class_change[i] = False
         else:
             count_fail +=1
+            class_change [i]= True
     print("Total instances:%d" %(count_pass+ count_fail))
     print("Number of fails:%d" %(count_fail))
     
+    for i in range(len(ground)):
+        if (gen_error[i]<= error_threshold):
+            if class_change[i]==True:
+                count_cc_lt +=1
+            else:
+                count_cnc_lt +=1
+        elif (gen_error[i]> error_threshold):
+            if class_change[i]==True:
+                count_cc_gt +=1
+            else:
+                count_cnc_gt +=1
+    print("Distribution of instances: cc_lt[%d] cnc_lt[%d] cc_gt[%d] cnc_gt[%d]" %(count_cc_lt, count_cnc_lt, count_cc_gt, count_cnc_gt))
 
 if __name__ == '__main__':
     main()
