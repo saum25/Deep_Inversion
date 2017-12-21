fixing normalisation issue for SLIC

randomise.py
@@ -7,7 +7,7 @@ Created on 21 Dec 2017
 import numpy as np
 from skimage.segmentation import slic
 
-def random_selection(mel_pred, norm_inv, random_bin = False, random_block = False, debug_print = False):
+def random_selection(norm_inv, random_bin = False, random_block = False, debug_print = False):
     
     n_bin_enabled = (norm_inv == 1).sum()
     n_bins = norm_inv.shape[0] * norm_inv.shape[1]
@@ -26,7 +26,7 @@ def random_selection(mel_pred, norm_inv, random_bin = False, random_block = Fals
         # keep the area covered nearly similar and use it to select N blocks randomly from a total of M
         M = 24 # inspired from SLIME for ease of implementation, i.e. divide 115 x 80 matrix into 24 segments
         N = int(np.floor(area_enabled * M))  # kept floor just to be a little miser on the blocks enabled
-        segments = slic(mel_pred, n_segments= M, compactness=1)
+        segments = slic(norm_inv, n_segments= M, compactness=1)
         n_features = np.unique(segments).shape[0]
         ones = np.random.choice(range(M), N, replace = False)    # non-repetitive random integers, selects indices randomly that are to be masked
         if debug_print: